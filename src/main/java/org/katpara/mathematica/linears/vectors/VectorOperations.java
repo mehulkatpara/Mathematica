c72ec26a39d99d48f23ee3eefe061411b3654cf2
Mehul Katpara <mkatpara19@gmail.com>
2020-07-01 21:29:44 -0400

    added comments in vector operations

--- src/main/java/org/katpara/mathematica/linears/vectors/VectorOperations.java
+++ src/main/java/org/katpara/mathematica/linears/vectors/VectorOperations.java
@@ -5,10 +5,25 @@ import org.katpara.mathematica.exceptions.InvalidVectorDimension;
 
 import java.util.List;
 
+/**
+ * The class provides a way to manipulate 2 or more vectors.
+ * All the methods in the class are immutable, at no point
+ * any of the method will modify the given vector, it will
+ * simply return a new vector at the end of the calculation.
+ *
+ * @author Mehul Katpara <mkatpara19@gmail.com>
+ * @since 1.0.0
+ */
 public final class VectorOperations {
     @java.io.Serial
     private static final long serialVersionUID = 4042529171225355742L;
 
+    /**
+     * The method will return an inverse vector if a given vector.
+     *
+     * @param v the vector
+     * @return the inverse vector of a given vector.
+     */
     public static Vector getInverseVector(final Vector v) {
         Number[] e = v.getElements(), n = new Number[e.length];
         for (int i = 0; i < e.length; i++)
@@ -17,29 +32,70 @@ public final class VectorOperations {
         return new ArrayVector(n);
     }
 
+    /**
+     * The method will add two vectors.
+     *
+     * @param v1 the base vector
+     * @param v2 the vector to be added
+     * @return the resulting vector
+     */
     public static Vector addVector(final Vector v1, final Vector v2) {
         return addSubVector(v1, v2, true);
     }
 
+    /**
+     * The method will subtract a vector from a vector.
+     *
+     * @param v1 the base vector
+     * @param v2 the vector to be subtracted
+     * @return the resulting vector
+     */
     public static Vector subtractVector(final Vector v1, final Vector v2) {
         return addSubVector(v1, v2, false);
     }
 
+    /**
+     * The method will add a list of vectors together and will
+     * return a resulting vector.
+     *
+     * @param vl the list of vectors
+     * @return the resulting vector after adding all
+     */
     public static Vector addVectors(final List<? extends Vector> vl) {
         return addSubVectors(vl, true);
     }
 
+    /**
+     * The method will subtract a list of vectors from the first vector,
+     * and return a resulting vector.
+     *
+     * @param vl the list of vectors
+     * @return the resulting vector after subtracting all from the base vector
+     */
     public static Vector subtractVectors(final List<? extends Vector> vl) {
         return addSubVectors(vl, false);
     }
 
 
+    /**
+     * The method will transpose vector to another dimension.
+     * If the given dimension is less then 2 or the same as
+     * the vector's dimension then the method will throw
+     * {@link InvalidVectorDimension} exception.
+     *
+     * @param v the vector
+     * @param d dimension to be transposed
+     * @return the transposed vector
+     * @throws InvalidVectorDimension when the given dimension is less
+     *                                than 2 or the same as the given
+     *                                vector dimension
+     */
     public static Vector transposeDimensions(final Vector v, final int d) {
         if (d < 2 || v.getDimension() == d)
             throw new InvalidVectorDimension();
 
         Number[] n = new Number[d], e = v.getElements();
-        if(d < v.getDimension())
+        if (d < v.getDimension())
             System.arraycopy(e, 0, n, 0, d);
         else {
             System.arraycopy(e, 0, n, 0, e.length);
