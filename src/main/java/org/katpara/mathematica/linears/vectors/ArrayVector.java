db27d74821eb321694c9b8d06abcc780df3feae3
Mehul Katpara <mkatpara19@gmail.com>
2020-07-10 15:36:40 -0400

    updated tests

--- src/main/java/org/katpara/mathematica/linears/vectors/ArrayVector.java
+++ src/main/java/org/katpara/mathematica/linears/vectors/ArrayVector.java
@@ -3,6 +3,7 @@ package org.katpara.mathematica.linears.vectors;
 import org.katpara.mathematica.exceptions.InvalidParameterProvidedException;
 import org.katpara.mathematica.exceptions.linears.InvalidVectorDimensionException;
 import org.katpara.mathematica.exceptions.NullArgumentProvidedException;
+import org.katpara.mathematica.linears.annotations.Angle;
 
 import java.util.*;
 import java.util.stream.Collectors;
